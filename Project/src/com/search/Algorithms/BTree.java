04dd76090dfedf6c3199e403ab0f23568ad18d7b
Super-ming-666 <supermingzero@gmail.com>
2021-03-25 23:27:40 +0800

    #897

---
+++ Project/src/com/search/Algorithms/BTree.java
@@ -0,0 +1,22 @@
+package com.search.Algorithms;
+
+import com.search.Algorithms.interfaces.TreeInterface;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Deque;
+
+public class BTree<T extends Comparable<T>> implements TreeInterface<T> {
+
+    public BTree(int order) {
+
+    }
+
+
+    public boolean add(T value) {
+
+    }
+}
+
+
